d561c92363ac32ce69114a95638112a2744bf012
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2025-10-17 22:06:46 +0200

    build: update bindings

--- ethereum/bindings/src/bin/hopr-contract-addresses.rs
+++ ethereum/bindings/src/bin/hopr-contract-addresses.rs
@@ -9,8 +9,8 @@
 //!   "networks": {
 //!     "rotsee": {
 //!      "addresses": {
-//!        "announcements": "0x0347A131861881604EA841b678210ba60b8E6D82",
-//!        "channels": "0x2CCd294e00593CE482216b88F3B481dF5EEFf357",
+//!        "announcements": "0xe45A8DBDADafd86AB7e2368b4749864083331cFC",
+//!        "channels": "0x9680F21d4583ad27F2e6bc3005EDF120FFf057B7",
 //!        "module_implementation": "0x79C3bF06E96A9373765284aFb0a35e9529E2B3F2",
 //!        "node_safe_migration": "0xe9670B5D87c87111C9050D915971B73b9f5021a9",
 //!        "node_safe_registry": "0xaFa257f2799835D2E29e7eC7ee448530F9d8Cb20",
@@ -25,16 +25,15 @@
 //!     "debug-staging": {
 //!       "indexer_start_block_number": 29690235,
 //!       "addresses": {
-//!         "announcements": "0xD78BCa8452B8Ea281a659f380E0eF710C64EB85b",
-//!         "channels": "0xc060582564b12335cD804339842F5509dbF6F74d",
-//!         "network_registry": "0xf08E27C3A09627D605bFd164459f7caF18D1d25f",
-//!         "network_registry_proxy": "0x0D1a8f1b13FD1d64696c5E03dd45Cd139e40dE0c",
-//!         "token": "0xD4fdec44DB9D44B8f2b6d529620f9C0C7066A2c1",
-//!         "node_stake_v2_factory": "0x5F5b459db681996292Ad58cc10e88027033149B8",
-//!         "node_safe_registry": "0x0bf6bd25ac47FE9D41A99B135Cb439B89138F05a",
-//!         "module_implementation": "0xe8d914Ef66b4FF086C6fbCb1E0ea97c0A9D2f3de",
-//!         "ticket_price_oracle": "0x281a91FeA199a3bAB5D7d5f05833B257E2fd7741",
-//!         "winning_probability_oracle": "0x02e1009fd222917Ee7bdfdBF8AE1e56c4ae3F6f3"
+//!         "announcements": "0x5cE40E4b330eA20d9217f0120E3329b98017f9Fb",
+//!         "channels": "0x09b77F370c2F379F3dB42B799fa7EDBB37D9E51a",
+//!         "module_implementation": "0x4db69d5D0cCaD110a6F327bc837fe5B7e1ffBDAe",
+//!         "node_safe_migration": "0x6E3Ef29b17FB33C1db372b06b19CE937894B317A",
+//!         "node_safe_registry": "0x549DCe2Ce8ba1c177B080D372580fa4d85123671",
+//!         "node_stake_factory": "0x87e1EBD42F0F79d88E5af212D0c24dc155EA78C0",
+//!         "ticket_price_oracle": "0x344f8DD6dBCd5Bc0B396dDD47F20FDc9A89c3c90",
+//!         "token": "0x02e1009fd222917Ee7bdfdBF8AE1e56c4ae3F6f3",
+//!         "winning_probability_oracle": "0xF2d64bb29A5207DFCF6185161e43364D3B2D9A27"
 //!       }
 //!     }
 //!   }
