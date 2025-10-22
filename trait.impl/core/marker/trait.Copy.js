f9c1b59bb05d30ef7972954059238d64c879bd28
Teebor-Choka <Teebor-Choka@users.noreply.github.com>
2025-10-22 09:06:27 +0000

    deploy: 9f03a48355dc15afce345b4afabca0949876a7d9

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hopr_bindings",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"hopr_bindings/config/struct.ContractAddresses.html\" title=\"struct hopr_bindings::config::ContractAddresses\">ContractAddresses</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"hopr_bindings/config/struct.SingleNetworkContractAddresses.html\" title=\"struct hopr_bindings::config::SingleNetworkContractAddresses\">SingleNetworkContractAddresses</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[672]}
---
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hopr_bindings",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"hopr_bindings/config/struct.NetworksWithContractAddresses.html\" title=\"struct hopr_bindings::config::NetworksWithContractAddresses\">NetworksWithContractAddresses</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[380]}
