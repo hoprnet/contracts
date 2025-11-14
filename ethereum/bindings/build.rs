f7da46b19c9eb41ee8d0d59791ff5c7cb09cc37c
Tino Breddin <t@tolbrino.com>
2025-11-14 09:10:02 +0100

    fix(contracts): Move addresses file into bindings

--- ethereum/bindings/build.rs
+++ ethereum/bindings/build.rs
@@ -3,14 +3,13 @@ use std::{env, fs, path::Path};
 use anyhow::Context;
 
 const CONTRACTS_ADDRESSES_FILENAME: &str = "contracts-addresses.json";
-const CONTRACTS_ADDRESSES_PATH: &str = const_format::formatcp!("../contracts/{}", CONTRACTS_ADDRESSES_FILENAME);
 
 fn main() -> anyhow::Result<()> {
     let out_dir = env::var("OUT_DIR").context("OUT_DIR environment variable should be set")?;
     let src_dir = env::var("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR environment variable should be set")?;
 
     let dest_path = Path::new(&out_dir).join(CONTRACTS_ADDRESSES_FILENAME);
-    let config_path = Path::new(&src_dir).join(CONTRACTS_ADDRESSES_PATH);
+    let config_path = Path::new(&src_dir).join(CONTRACTS_ADDRESSES_FILENAME);
 
     if !config_path.exists() {
         return Err(anyhow::anyhow!(
@@ -19,12 +18,12 @@ fn main() -> anyhow::Result<()> {
             config_path
         ));
     } else {
-        fs::copy(config_path, &dest_path)
+        fs::copy(config_path.clone(), &dest_path)
             .context(format!("Failed to copy {CONTRACTS_ADDRESSES_FILENAME} to OUT_DIR"))?;
     }
 
     // Tell Cargo to rerun this build script if the config file changes
-    println!("cargo:rerun-if-changed={CONTRACTS_ADDRESSES_PATH}");
+    println!("cargo:rerun-if-changed={:#?}", config_path);
 
     Ok(())
 }
