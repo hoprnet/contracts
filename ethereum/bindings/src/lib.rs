a99f0dc2eca7a417c5f293255fd205239e6ebfb5
QYuQianchen <10935300+QYuQianchen@users.noreply.github.com>
2025-12-22 18:42:50 +0100

    feat: remove extra features to first unblock blokli tests

--- ethereum/bindings/src/lib.rs
+++ ethereum/bindings/src/lib.rs
@@ -12,12 +12,8 @@ pub mod constants;
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub use codegen::*;
 
-pub mod errors;
 pub mod exports {
     pub use alloy;
 }
-
-pub mod methods;
-pub mod utils;
 
 pub use config::{CONTRACTS_ADDRESSES_FILE_CONTENT, ContractAddresses};
